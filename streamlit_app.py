84066acf91381cd729b07106cf559caf4a6b08bb
ShivaSuryaBizA <130978745+ShivaSuryaBizA@users.noreply.github.com>
2023-04-28 01:17:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-#import streamlit 
-#import pandas
-#import snowflake.connector
-#import requests
+import streamlit 
+import pandas
+import snowflake.connector
+import requests
 from urllib.error import URLError
 
 streamlit.title('My Parent New Healthy Diner')
