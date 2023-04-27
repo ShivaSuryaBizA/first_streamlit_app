0d4af9eb6fb03c612fc6e1621ea6132e72a9092c
ShivaSuryaBizA <130978745+ShivaSuryaBizA@users.noreply.github.com>
2023-04-28 01:32:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
@@ -33,8 +33,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -42,6 +40,12 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('app_my_fruit')")
+try:
+  add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+  if not app_my_fruit:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    streamlit.write('Thanks for adding ', add_my_fruit)
+    my_cur.execute("insert into fruit_load_list values ('app_my_fruit')")
+except URLError as e:
+  streamlit.error() 
